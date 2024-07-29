70758c16c166de0af8575072a0ebb05b824f3442
Vverdy <46199406+F1encko627@users.noreply.github.com>
2024-07-29 14:52:05 +0300

    QoL, refactoring
    Counter of filtered IPs
    Better Label and Placeholders
    Refactoring of fields updaters

--- src/components/NmapProcessor.vue
+++ src/components/NmapProcessor.vue
@@ -9,7 +9,7 @@
         <q-radio dense v-model="filterMode" val="and" label="И" class="text-subtitle2" />
         <q-radio dense v-model="filterMode" val="or" label="ИЛИ" class="text-subtitle2" />
       </div>
-      <q-icon v-if="ports !== ''" name="clear" @click="ports = ''" class="cursor-pointer" />
+      <q-icon v-if="ports !== ''" name="clear" @click="ports=''" class="cursor-pointer" />
     </template>
 
     <template v-slot:after>
@@ -23,14 +23,14 @@
     color="sred-12" label="Сырой Nmap" placeholder="Вставьте вывод Nmap сюда">
 
     <template v-slot:append>
-      <q-icon v-if="RawNmapOutput" name="delete" @click.stop.prevent="RawNmapOutput = ''" class="cursor-pointer" />
+      <q-icon v-if="RawNmapOutput" name="delete" @click.stop.prevent="clear" class="cursor-pointer" />
     </template>
   </q-input>
 
-  <q-input v-if="showParsed" class="full-height	" v-model="nmapParsedCopy" filled type="textarea" color="shred-12"
-    label="Обработанный Nmap" :placeholder="nmapParsed">
+  <q-input v-if="showParsed" v-model="nmapParsedCopy" filled type="textarea" color="shred-12"
+    :label="filterLabel" :placeholder="nmapParsed">
     <template v-slot:append>
-      <q-icon v-if="nmapParsed !== ''" name="delete" @click="nmapParsed = ''; RawNmapOutput = ''; showParsed = false"
+      <q-icon v-if="nmapParsedCopy != '' || RawNmapOutput" name="delete" @click="clear"
         class="cursor-pointer" />
     </template>
   </q-input>
@@ -43,6 +43,8 @@ const RawNmapOutput = ref('')
 const nmapParsed = ref('') // "Блокнот" пользователя
 const nmapParsedCopy = ref('') // Для быстрого восстановления
 const showParsed = ref(false)
+const filterCounter = ref(0)
+const filterLabel = ref('Обработанный Namp')
 const ports = ref('')
 const filterMode = ref('and')
 const errDetected = ref(false)
@@ -52,17 +54,21 @@ const parser = watch([RawNmapOutput, filterMode, ports], () => {
   parseNmap()
 })
 
-// Обновление демонстрации, если новый результат
-const updater = watch(nmapParsed, () => {
-  nmapParsedCopy.value = nmapParsed.value
-})
-
 const refreshAction = () => {
   if (RawNmapOutput.value == '') {
     return
   }
   nmapParsedCopy.value = nmapParsed.value
   showParsed.value = true
+}
+
+const clear = () => {
+  console.log('action')
+  filterLabel.value = 'Обработанный Nmap'
+  nmapParsed.value = ''
+  nmapParsedCopy.value = ''
+  RawNmapOutput.value = ''
+  showParsed.value = false
 }
 
 const parseNmap = () => {
@@ -84,21 +90,11 @@ const parseNmap = () => {
   let checkForPorts = ports.value.length == 0 ? '554' : ports.value
 
   nmapParsed.value = ''
+  filterCounter.value = 0
 
   RawNmapOutput.value.split('\n').forEach(line => {
-    // Запоминание открытых портов
-    if (currentIP && line.toLowerCase().includes('open')) {
-      checkForPorts.split(' ').forEach(port => {
-        // Доп. символы чтобы не было путаницы с 80/http и 8080/http и т.п.
-        if ((' ' + line).includes(' ' + port.toString() + '/')) {
-          portsFound += port + ', '
-          return
-        }
-      })
-    }
-
-    // Вывод строки если были данные, сброс памяти и запоминание новго IP
-    if (line.substring(0, 5).includes('Nmap ')) {
+    // Вывод строки если были данные, сброс памяти и/или запоминание новго IP
+    if (line.includes('Nmap scan report for ')) {
       if (portsFound != '') {
         if (filterMode.value == 'and') {
           checkForPorts.split(' ').forEach(port => {
@@ -110,20 +106,35 @@ const parseNmap = () => {
 
         if (foundAll) {
           nmapParsed.value += currentIP += ': ' + portsFound.substring(0, portsFound.length - 2) + '\n'
+          ++filterCounter.value
         }
 
         foundAll = true
         portsFound = ''
       }
-
       currentIP = line.substring(21)
+      return
+    }
+
+    // Запоминание открытых портов
+    if (currentIP && line.includes('open')) {
+      checkForPorts.split(' ').forEach(port => {
+        // Доп. символы чтобы не было путаницы с 80/http и 8080/http и т.п.
+        if ((' ' + line).includes(' ' + port.toString() + '/')) {
+          portsFound += port + ', '
+          return
+        }
+      })
     }
   });
 
+  // Действия для вывода результата
   if (nmapParsed.value.length == 0) {
     nmapParsed.value = 'Совпадений не найдено'
   }
 
+  nmapParsedCopy.value = nmapParsed.value
+  filterLabel.value = 'Отфильтровано: ' + filterCounter.value
   showParsed.value = true
 }
 </script>
